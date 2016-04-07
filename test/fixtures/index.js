54738be6b5b2c38b0e77663561856f9e90399e99
Thiago Felix <thiago@thiagofelix.com>
2016-04-07 15:19:41 -0300

    refactor(tests): extract repository under test into the fixture folder

---
+++ test/fixtures/index.js
@@ -0,0 +1,10 @@
+module.exports.repo = {
+  owner: 'stevemaotest',
+  name: 'conventional-github-releaser-test',
+  pkg: { path: __dirname + '/_package.json' }
+}
+
+module.exports.auth = {
+  type: 'oauth',
+  token: process.env.TEST_CONVENTIONAL_GITHUB_RELEASER_TOKEN
+};
