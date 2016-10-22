53ece4dee128d9214d6e8f54f46f45d1adc565c7
Hutson Betts <hutson@hyper-expanse.net>
2016-10-22 09:08:18 -0500

    fix(transform): only match semantic version tags
    `conventional-changelog-writer` expects that the `version` property of a
    commit object is a semantically valid version.
    
    The previous version of the `transform` function would match against any
    tag associated with the commit, and assign the first matched tag as the
    commit's version.
    
    If the matched tag is not a semantically valid version number,
    `conventional-changelog-writer` will ignore the `version` property of
    the commit object, and not successfully generate a changelog (since it
    has no version on which to generate the changelog).
    
    Therefore we update the `transform` function to only match against
    semantically valid version tags when assigning the `version` property of
    a commit object.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 var assign = require('object-assign');
 var conventionalChangelog = require('conventional-changelog');
-var dateFormat = require('dateformat');
 var Github = require('github');
 var gitSemverTags = require('git-semver-tags');
 var merge = require('lodash.merge');
 var Q = require('q');
 var semver = require('semver');
 var through = require('through2');
+var transform = require('./lib/transform');
 
 var github = new Github({
   version: '3.0.0'
@@ -40,20 +40,7 @@ function conventionalGithubReleaser(auth, changelogOpts, context, gitRawCommitsO
   writerOpts = changelogArgs[4];
 
   changelogOpts = merge({
-    transform: function(chunk, cb) {
-      if (typeof chunk.gitTags === 'string') {
-        var match = /tag:\s*(.+?)[,\)]/gi.exec(chunk.gitTags);
-        if (match) {
-          chunk.version = match[1];
-        }
-      }
-
-      if (chunk.committerDate) {
-        chunk.committerDate = dateFormat(chunk.committerDate, 'yyyy-mm-dd', true);
-      }
-
-      cb(null, chunk);
-    },
+    transform: transform,
     releaseCount: 1
   }, changelogOpts);
 
